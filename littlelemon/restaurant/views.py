f33a4c09c61c881d035a640c39db91cf053daa3c
Mohammed Wasid <wasid2345@gmail.com>
2024-05-27 11:28:37 -0400

    Added home/index.html and static files

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def sayHello(request):
+    return HttpResponse('Hello World')
+
+def home(request):
+    return render(request, 'index.html', {})
