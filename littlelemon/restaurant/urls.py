b7ac1f3dc228336d10e4b8cd0fed9d16697c0a31
Mohammed Wasid <wasid2345@gmail.com>
2024-05-30 19:13:06 -0400

    created views and serializers for menu and booking

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('hello', views.sayHello, name='sayHello'),
+    path('menu', views.MenuItemView.as_view(), name ='menuItemView'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='singleMenuItemView'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework import generics, viewsets
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -9,3 +11,24 @@ def sayHello(request):
 
 def home(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return []
+        else:
+            return [IsAuthenticated()]
+        
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+    
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+    
