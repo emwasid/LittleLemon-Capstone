158394bacc84c09dc28ab9857ed93314ecbe7779
Mohammed Wasid <wasid2345@gmail.com>
2024-06-03 12:03:29 -0400

    added test cases in app folder but should be changed to project folder soon

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = MenuItem.objects.create(title="IceCream", price=80, inventory=100)
+        itemstr = item.get_item()
+        
+        self.assertEqual(itemstr, "IceCream : 80")
+
+    def test_str(self):
+        item = MenuItem.objects.create(title="Baklava", price=10, inventory=200)
+        itemstr = item.__str__()
+        
+        self.assertEqual(itemstr, "Baklava")
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -7,9 +7,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', views.home, name='home'),
     path('hello', views.sayHello, name='sayHello'),
-    path('menu', views.MenuItemView.as_view(), name ='menuItemView'),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='singleMenuItemView'),
+    path('menu-items', views.MenuItemView.as_view(), name ='menuItemView'),
+    path('menu-items/<int:pk>', views.SingleMenuItemView.as_view(), name='singleMenuItemView'),
     path('message', views.msg, name='msg'),
     
-    path('api-token-auth', obtain_auth_token),
+    path('api-token-auth', obtain_auth_token), #pass username and password in a post method in insomnia to get token
 ]
